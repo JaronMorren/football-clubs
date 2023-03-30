8616a5ad486aeb7e03efb50479f919f7331e4123
jaron morren <jaronmorren@gmail.com>
2023-03-29 21:05:10 -0300

    Created TeamsModel

---
+++ app/backend/src/database/models/TeamsModel.ts
@@ -0,0 +1,43 @@
+import { Model, STRING, INTEGER } from 'sequelize';
+import db from '.';
+// import OtherModel from './OtherModel';
+
+class Teams extends Model {
+  declare id: number;
+  declare teamName: string;
+}
+
+Teams.init({
+  id: {
+    autoIncrement: true,
+    allowNull: false,
+    type: INTEGER,
+    primaryKey: true,
+  },
+  teamName: {
+    allowNull: false,
+    type: STRING,
+
+  },
+  // ... Campos
+}, {
+  // ... Outras configs
+  underscored: true,
+  sequelize: db,
+  tableName: 'teams',
+  // modelName: 'example',
+  timestamps: false,
+});
+
+/**
+  * `Workaround` para aplicar as associations em TS:
+  * Associations 1:N devem ficar em uma das instâncias de modelo
+  * */
+
+// OtherModel.belongsTo(Example, { foreignKey: 'campoA', as: 'campoEstrangeiroA' });
+// OtherModel.belongsTo(Example, { foreignKey: 'campoB', as: 'campoEstrangeiroB' });
+
+// Example.hasMany(OtherModel, { foreignKey: 'campoC', as: 'campoEstrangeiroC' });
+// Example.hasMany(OtherModel, { foreignKey: 'campoD', as: 'campoEstrangeiroD' });
+
+export default Teams;
