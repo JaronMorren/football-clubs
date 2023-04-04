66d94696f9041ab193fcfb1680ff8f79698c7164
jaron morren <jaronmorren@gmail.com>
2023-04-03 22:38:05 -0300

    Created Teams service layer

---
+++ app/backend/src/database/services/TeamsService.ts
@@ -0,0 +1,18 @@
+import { ModelStatic } from 'sequelize';
+import Teams from '../models/TeamsModel';
+import ITeams from '../interfaces/ITeams';
+
+class TeamsService {
+  teamsModel : ModelStatic<Teams>;
+
+  constructor(teamsModel:ModelStatic<Teams>) {
+    this.teamsModel = teamsModel;
+  }
+
+  public async getAllTeams(): Promise<ITeams[]> {
+    const allTeams = await this.teamsModel.findAll();
+    return allTeams;
+    //  I based this modelstatic interface on this thread: https://trybecourse.slack.com/archives/C03MSCCRPAQ/p1680107639238259
+  }
+}
+export default TeamsService;
