2b2630cc45eb448fba74708b07ce32138e86cd8a
jaron morren <jaronmorren@gmail.com>
2023-04-09 15:27:02 -0300

    fixed POST/login route

--- app/backend/src/database/routes/LoginRouter.ts
+++ app/backend/src/database/routes/LoginRouter.ts
@@ -1,9 +1,12 @@
 import { Router } from 'express';
 import LoginController from '../controllers/LoginController';
+import validateLogin from '../middleware/loginValidations';
 
 const loginRouter = Router();
 const loginController = new LoginController();
 
-loginRouter.post('/', loginController.userLogin);
+loginRouter.post('/', validateLogin, (request, response) => {
+  loginController.userLogin(request, response);
+});
 
 export default loginRouter;
