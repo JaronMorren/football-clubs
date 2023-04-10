597f214ab10e1097af63f2fdfa72f12d92383cf2
jaron morren <jaronmorren@gmail.com>
2023-04-09 21:54:47 -0300

    Added get/role route to login router

--- app/backend/src/database/routes/LoginRouter.ts
+++ app/backend/src/database/routes/LoginRouter.ts
@@ -1,12 +1,17 @@
 import { Router } from 'express';
 import LoginController from '../controllers/LoginController';
 import validateLogin from '../middleware/loginValidations';
+import validateToken from '../middleware/tokenValidations';
 
 const loginRouter = Router();
 const loginController = new LoginController();
 
 loginRouter.post('/', validateLogin, (request, response) => {
   loginController.userLogin(request, response);
+});
+
+loginRouter.get('/role', validateToken, (request, response) => {
+  loginController.getUserRole(request, response);
 });
 
 export default loginRouter;
