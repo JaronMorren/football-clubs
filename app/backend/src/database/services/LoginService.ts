1e4379537c7fe71527903758bfadcd983cd1f894
jaron morren <jaronmorren@gmail.com>
2023-04-11 23:48:33 -0300

    removed password injection, refactored code, moved error messages to controller layer

--- app/backend/src/database/services/LoginService.ts
+++ app/backend/src/database/services/LoginService.ts
@@ -1,22 +1,22 @@
-import * as bcrypt from 'bcryptjs';
 import { ModelStatic } from 'sequelize';
 import Users from '../models/UsersModel';
+import { IUsers } from '../interfaces/IUsers';
 
 class LoginService {
   model: ModelStatic<Users> = Users;
 
-  public async userLogin(email: string, password: string) {
+  public async getUserByEmail(email: string) : Promise<IUsers | null> {
     const user = await this.model.findOne({ where: { email } });
-    if (!user) {
-      return { type: 401, message: 'Invalid email or password' };
-    }
-    const isCorrectPassword = bcrypt.compareSync(password, user.dataValues.password);
-    if (!isCorrectPassword) {
-      return { type: 401, message: 'Invalid email or password' };
-    }
-    if (isCorrectPassword) {
-      return { type: null, message: user.role };
-    }
+    return user;
+  }
+
+  public async getUserByID(id: number): Promise<IUsers | null> {
+    const user = await this.model.findByPk(
+      id,
+      { attributes: { exclude: ['password', 'id', 'username', 'email'] } },
+    );
+    return user;
+    // this function fetches users by their id via the find by primary key method and excludes their sensitive information like password etc.
   }
 }
 // Daniel Röhe and Ligia Bicalho helped me write this function
