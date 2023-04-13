dba73ba8f4724fc197d1dbf7b55f4a524fdda28c
jaron morren <jaronmorren@gmail.com>
2023-04-12 22:58:46 -0300

    standardised matches router

--- app/backend/src/database/routes/MatchesRouter.ts
+++ app/backend/src/database/routes/MatchesRouter.ts
@@ -2,8 +2,8 @@ import { Router } from 'express';
 import MatchesController from '../controllers/MatchesController';
 
 const matchesRouter = Router();
-
 const matchesController = new MatchesController();
+
 matchesRouter.get('/', matchesController.getAllMatches);
 
 export default matchesRouter;
