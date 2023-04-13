69fd08c13dc751d3b6f5fd381e4a924104f1aa0f
jaron morren <jaronmorren@gmail.com>
2023-04-12 23:34:59 -0300

    created patch id finish route in the matches router

--- app/backend/src/database/routes/MatchesRouter.ts
+++ app/backend/src/database/routes/MatchesRouter.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 import MatchesController from '../controllers/MatchesController';
+import validateToken from '../middleware/tokenValidations';
 
 const matchesRouter = Router();
 const matchesController = new MatchesController();
 
 matchesRouter.get('/', matchesController.getAllMatches);
+matchesRouter.patch('/:id/finish', validateToken, matchesController.finishMatch);
 
 export default matchesRouter;
