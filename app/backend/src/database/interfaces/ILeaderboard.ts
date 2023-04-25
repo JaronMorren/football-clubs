2f40ab3a77ea4072aa178228fa1f534e35fbdd77
jaron morren <jaronmorren@gmail.com>
2023-04-24 21:28:03 -0300

    added leaderboard interface

---
+++ app/backend/src/database/interfaces/ILeaderboard.ts
@@ -0,0 +1,11 @@
+interface ILeaderboard {
+  name: string,
+  totalPoints: number,
+  totalGames: number,
+  totalVictories: number,
+  totalDraws: number,
+  totalLosses: number,
+  goalsFavor: number,
+  goalsOwn: number,
+}
+export default ILeaderboard;
