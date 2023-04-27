8e1f4247d257bc804d6546a121435b07f05e6d70
Jaron Morren <jaronmorren@gmail.com>
2023-04-27 20:46:27 -0300

    corrected lint errors

--- app/backend/src/database/services/LeaderboardService.ts
+++ app/backend/src/database/services/LeaderboardService.ts
@@ -1,10 +1,16 @@
 import ILeaderboard from '../interfaces/ILeaderboard';
 import sequelize from '../models';
+import awayLeaderboard from '../queries/awayLeaderboardQuery';
 import homeLeaderboard from '../queries/homeLeaderboardQuery';
 
 class LeaderboardService {
   public getHomeLeaderboard = async (): Promise<ILeaderboard[]> => {
     const [leaderboard] = await sequelize.query(homeLeaderboard);
+    return leaderboard as ILeaderboard[];
+  };
+
+  public getAwayLeaderboard = async (): Promise<ILeaderboard[]> => {
+    const [leaderboard] = await sequelize.query(awayLeaderboard);
     return leaderboard as ILeaderboard[];
   };
 }
