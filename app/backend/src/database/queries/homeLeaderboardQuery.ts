452d99ca5618660f74211d1b12e886b19a63e87b
Jaron Morren <jaronmorren@gmail.com>
2023-04-27 16:05:21 -0300

    added goalsbalance and efficiency

--- app/backend/src/database/queries/homeLeaderboardQuery.ts
+++ app/backend/src/database/queries/homeLeaderboardQuery.ts
@@ -1,31 +1,37 @@
 const homeLeaderboard = `
 SELECT 
-teams.team_name AS name,
-SUM(CASE
-    WHEN matches.home_team_goals > matches.away_team_goals THEN 3
-    WHEN matches.home_team_goals = matches.away_team_goals THEN 1
-    ELSE 0
-END) AS totalPoints,
-COUNT(*) AS totalGames,
-SUM(CASE
-    WHEN matches.home_team_goals > matches.away_team_goals THEN 1
-    ELSE 0
-END) AS totalVictories,
-SUM(CASE
-    WHEN matches.home_team_goals = matches.away_team_goals THEN 1
-    ELSE 0
-END) AS totalDraws,
-SUM(CASE
-    WHEN matches.home_team_goals < matches.away_team_goals THEN 1
-    ELSE 0
-END) AS totalLosses,
-SUM(matches.home_team_goals) AS goalsFavor,
-SUM(matches.away_team_goals) AS goalsOwn
+    teams.team_name AS name,
+    SUM(CASE
+        WHEN matches.home_team_goals > matches.away_team_goals THEN 3
+        WHEN matches.home_team_goals = matches.away_team_goals THEN 1
+        ELSE 0
+    END) AS totalPoints,
+    COUNT(*) AS totalGames,
+    SUM(CASE
+        WHEN matches.home_team_goals > matches.away_team_goals THEN 1
+        ELSE 0
+    END) AS totalVictories,
+    SUM(CASE
+        WHEN matches.home_team_goals = matches.away_team_goals THEN 1
+        ELSE 0
+    END) AS totalDraws,
+    SUM(CASE
+        WHEN matches.home_team_goals < matches.away_team_goals THEN 1
+        ELSE 0
+    END) AS totalLosses,
+    SUM(matches.home_team_goals) AS goalsFavor,
+    SUM(matches.away_team_goals) AS goalsOwn,
+    SUM(home_team_goals) - SUM(away_team_goals) AS goalsBalance,
+    FORMAT((SUM(CASE
+            WHEN matches.home_team_goals > matches.away_team_goals THEN 3
+            WHEN matches.home_team_goals = matches.away_team_goals THEN 1
+            ELSE 0
+        END) / (COUNT(matches.id) * 3)) * 100,
+        2) AS efficiency
 FROM
-TRYBE_FUTEBOL_CLUBE.teams AS teams
-    JOIN
-TRYBE_FUTEBOL_CLUBE.matches AS matches ON matches.home_team_id = teams.id
-    AND matches.in_progress IS FALSE
-GROUP BY name
-`;
+    TRYBE_FUTEBOL_CLUBE.teams AS teams
+        JOIN
+    TRYBE_FUTEBOL_CLUBE.matches AS matches ON matches.home_team_id = teams.id
+        AND matches.in_progress IS FALSE
+GROUP BY name;`
 export default homeLeaderboard;
